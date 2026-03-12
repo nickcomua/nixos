3e43ff2f0b4c1397ac956ae719b26f162d713c9e
Mykola Korniichuk <mykorniichuk@opt-net.eu>
2026-03-12 11:44:25 +0100

    fix comin

--- comin/flake.nix
+++ comin/flake.nix
@@ -1,53 +1,78 @@
 {
   inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1";
+    nixos.url = "path:..";
     comin = {
       url = "github:nlewo/comin";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixos/nixpkgs";
     };
   };
   outputs = {
-    self,
-    nixpkgs,
+    nixos,
     comin,
+    ...
   }: {
-    nixosConfigurations = {
-      nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          comin.nixosModules.comin
-          {
-            services.comin = {
-              enable = true;
-              remotes = [
-                {
-                  name = "origin";
-                  url = "https://github.com/nickcomua/nixos.git";
-                  branches.main.name = "stable-nixos";
-                }
-              ];
-            };
-          }
-        ];
-      };
+    nixosConfigurations.nixos = nixos.nixosConfigurations.nixos.extendModules {
+      modules = [
+        comin.nixosModules.comin
+        {
+          services.comin = {
+            enable = true;
+            repositorySubdir = "comin";
+            remotes = [
+              {
+                name = "origin";
+                url = "https://github.com/nickcomua/nixos.git";
+                branches.main.name = "stable-nixos";
+              }
+            ];
+          };
+        }
+      ];
     };
-  };
-  nixConfig = {
-    extra-trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
-    ];
-    extra-substituters = [
-      "https://cache.nixos.org"
-      "https://nix-community.cachix.org"
-      "https://devenv.cachix.org"
-      "https://cache.garnix.io"
-    ];
-    extra-experimental-features = [
-      "nix-command"
-      "flakes"
-    ];
+
+    nixosConfigurations.alta = nixos.nixosConfigurations.alta.extendModules {
+      modules = [
+        comin.nixosModules.comin
+        {
+          services.comin = {
+            enable = true;
+            repositorySubdir = "comin";
+            remotes = [
+              {
+                name = "origin";
+                url = "https://github.com/nickcomua/nixos.git";
+                branches.main.name = "stable-alta";
+              }
+            ];
+          };
+        }
+      ];
+    };
+
+    darwinConfigurations."Mykolas-MacBook-Pro" = nixos.darwinConfigurations."Mykolas-MacBook-Pro".extendModules {
+      modules = [
+        comin.darwinModules.comin
+        ({lib, ...}: {
+          services.comin = {
+            enable = true;
+            hostname = "Mykolas-MacBook-Pro";
+            repositorySubdir = "comin";
+            remotes = [
+              {
+                name = "origin";
+                url = "https://github.com/nickcomua/nixos.git";
+                branches.main.name = "stable-darwin";
+              }
+            ];
+          };
+          # comin's darwin module accesses nix.package even when nix.enable = false
+          # (Determinate Nix case). Override the launchd daemon PATH manually.
+          launchd.daemons.comin.serviceConfig.EnvironmentVariables = lib.mkForce {
+            NIX_REMOTE = "daemon";
+            PATH = "/nix/var/nix/profiles/default/bin:/run/current-system/sw/bin:/usr/bin:/bin:/usr/sbin:/sbin";
+          };
+        })
+      ];
+    };
   };
 }

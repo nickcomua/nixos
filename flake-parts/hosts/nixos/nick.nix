3bee2428fa4750436ecb2a1c5ac2be5c1a28609a
Mykola Korniichuk <mykola.korniichuk.ua@gmail.com>
2026-04-09 10:53:18 +0200

    set chrome default browser

--- flake-parts/hosts/nixos/nick.nix
+++ flake-parts/hosts/nixos/nick.nix
@@ -18,6 +18,26 @@
   # Enable OpenClaw (config is in shared/clawdbot.nix)
   programs.openclaw.enable = true;
 
+  # Disable Horse Browser as default, use Google Chrome instead
+  programs.horse-browser.setAsDefault = false;
+
+  # Set Google Chrome as the default browser
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "google-chrome.desktop";
+      "text/xml" = "google-chrome.desktop";
+      "application/xhtml+xml" = "google-chrome.desktop";
+      "application/xml" = "google-chrome.desktop";
+      "application/vnd.mozilla.xul+xml" = "google-chrome.desktop";
+      "application/rss+xml" = "google-chrome.desktop";
+      "application/rdf+xml" = "google-chrome.desktop";
+      "x-scheme-handler/http" = "google-chrome.desktop";
+      "x-scheme-handler/https" = "google-chrome.desktop";
+      "x-scheme-handler/unknown" = "google-chrome.desktop";
+    };
+  };
+
   # Host-specific wayland settings
   wayland.hyprland.monitor = [
     "eDP-1,1920x1080@165,0x0,1" # Laptop display
